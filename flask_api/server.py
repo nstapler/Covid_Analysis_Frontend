e20e40e317a32dfad4421d34e6e09c39a034776e
nstapler <staplern@bu.edu>
2020-07-27 21:26:28 -0700

    Update db connect and queries

--- flask_api/server.py
+++ flask_api/server.py
@@ -26,17 +26,29 @@ def RequestInfoGivenRegion(query,Region):
     results = json.dumps(dictfetchall(cursor))
     return results
 
-
+# *****NOTICE******
+# Within the Administration tab on the left column of the mysql workbench
+# go to users and priveleges and add an account called public
+# use standard for the authentification type 
+# localhost for the limit to host matching
+# Covid_Analysis for the password
+# then go to the administrative roles for the public user
+# and click DBA for the role
+# then apply
 db = pymysql.connect(host = "localhost",
-                     user = "root",
+                     user = "public",
                      password = "Covid_Analysis",
                      db = "covid" )                 #gives access to script as root can change later
 cursor = db.cursor()                                #new cursor command so we dont have to write db.cursor() all the time
 
-ListOfQuereys=[
-"SELECT id, name FROM region_name",
-"SELECT name, MAX(confirmed), MAX(deaths), MAX(positive), MAX(suspected), MAX(icu_positive), MAX(icu_suspected) FROM region_name INNER JOIN total_count_confirmed ON region_name.id = total_count_confirmed.region_name_id INNER JOIN total_count_deaths ON region_name.id = total_count_deaths.region_name_id INNER JOIN covid19_positive_patients ON region_name.id = covid19_positive_patients.region_name_id INNER JOIN suspected_covid19_positive_patients ON region_name.id = suspected_covid19_positive_patients.region_name_id INNER JOIN icu_covid19_positive_patients ON region_name.id = icu_covid19_positive_patients.region_name_id INNER JOIN icu_covid19_suspected_patients ON region_name.id = icu_covid19_suspected_patients.region_name_id WHERE name = '{}' GROUP BY name;"
-]
+query_object = {
+    "get_regions":"SELECT id, name FROM region_name;",
+    "get_categories":"SELECT name, MAX(confirmed), MAX(deaths), MAX(positive), MAX(suspected), MAX(icu_positive), MAX(icu_suspected) FROM region_name INNER JOIN total_count_confirmed ON region_name.id = total_count_confirmed.region_name_id INNER JOIN total_count_deaths ON region_name.id = total_count_deaths.region_name_id INNER JOIN covid19_positive_patients ON region_name.id = covid19_positive_patients.region_name_id INNER JOIN suspected_covid19_positive_patients ON region_name.id = suspected_covid19_positive_patients.region_name_id INNER JOIN icu_covid19_positive_patients ON region_name.id = icu_covid19_positive_patients.region_name_id INNER JOIN icu_covid19_suspected_patients ON region_name.id = icu_covid19_suspected_patients.region_name_id WHERE name = '{}' GROUP BY name;"
+}
+
+# #uncomment below to test
+# JsonResults=RequestInfo(query_object["get_regions"])
+# print(JsonResults)
 #query = "SELECT id, name FROM region_name"          #specific query
 #cursor.execute(query)                               # do the query
 #Region = cursor.fetchall()                          # get all the results and place it into the tulep Region
@@ -48,12 +60,12 @@ def home():
 
 @app.route('/regions',methods=['GET'])
 def regions():
-    JsonResults=RequestInfo(ListOfQuereys[0])
+    JsonResults=RequestInfo(query_object["get_regions"])
     return(JsonResults)
 
 @app.route('/<string:Region>', methods=['GET'])
 def get_Region(Region):
-    data=RequestInfoGivenRegion(ListOfQuereys[1], Region)
+    data=RequestInfoGivenRegion(query_object["get_categories"], Region)
     return (data)         
 
 @app.route('/about') # about page
