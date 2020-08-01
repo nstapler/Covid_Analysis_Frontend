48021d1d4c273b89e264be7d882445b89342de77
nstapler <staplern@bu.edu>
2020-08-01 10:07:19 -0700

    Lint jsx files

--- src/App.js
+++ src/App.js
@@ -1,42 +1,35 @@
-import React, { useEffect, useState,Component } from 'react';
+import React, {Component } from 'react';
 import './App.css';
 import logo from './logo.svg';
 import FilterBar from './components/FilterBar';
 
 export class app extends Component {
-  // useEffect(() => {
-  //   fetch('/time').then(res => res.json()).then(data => {
-  //     setCurrentTime(data.time);
-  //   });
-  // }, []);
-  //Get Regions
-  //Get Region info
-  //Get Region Data/submit chosen region info
-  constructor(props){
+
+  constructor(props) {
     super(props);
     this.state = {
-        filterSelection:{},
-        regionData:{},
-        regions:[]
+      filterSelection: {},
+      regionData: {},
+      regions: []
     };
   }
 
 
-componentDidMount() {
-}
-componentWillUnmount() {
-}
-render() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-      <FilterBar regions={this.getRegions}></FilterBar>
-    </div>
-  );
-}
-  
+  componentDidMount() {
+  }
+  componentWillUnmount() {
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        <FilterBar regions={this.getRegions}></FilterBar>
+      </div>
+    );
+  }
+
 }
 
 export default app;
--- src/components/MainFilter.js
+++ src/components/MainFilter.js
@@ -1,38 +1,38 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export class MainFilter extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
-            value:""
+            value: ""
         };
     }
     componentDidMount() {
     }
     componentWillUnmount() {
-  }
-  handleChange(event) {
-    this.setState({value: event.target.value});
-    this.props.changeFunc(event.target.value);
-    this.props.setSelection(event.target.name,event.target.value);
-  }
+    }
+    handleChange(event) {
+        this.setState({ value: event.target.value });
+        this.props.changeFunc(event.target.value);
+        this.props.setSelection(event.target.name, event.target.value);
+    }
     render() {
         var regionSelection;
-        if(this.props.regions){
-            regionSelection = this.props.regions.map((region)=>{
-                return <option key={region} value={region}>{region}</option>;
-             });
+        if (this.props.regions) {
+            regionSelection = this.props.regions.map((region) => {
+                return (<option key={region} value={region}>{region}</option>);
+            });
         }
-        
+
         return (
             <label className="MainFilter">
                 <span>Choose The Region:</span>
                 <span>
-                <select name="name" value={this.state.value} onChange={this.handleChange.bind(this)}>
-                    <option value=""></option>
-                    {regionSelection}
-                </select>
-                    </span>
+                    <select name="name" value={this.state.value} onChange={this.handleChange.bind(this)}>
+                        <option value=""></option>
+                        {regionSelection}
+                    </select>
+                </span>
             </label>
         )
     }
